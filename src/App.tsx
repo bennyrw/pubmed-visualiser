5a0a88dc5a68bfe82408bff675411589acc8cb4e
Ben Morris <ben.morris@camcog.com>
2020-06-12 22:49:17 +0100

    Remove unused import

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Map } from 'immutable';
 
-import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
 
 import Search from './views/Search';
